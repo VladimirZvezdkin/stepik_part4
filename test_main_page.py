023cb8887d580a0016d7352813a6306571780e50
VladimirZvezdkin <vladimir-zvezdkin@yandex.ru>
2022-04-25 17:43:42 +0300

    moved test for login page in this file.

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,16 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
 
 
 def test_guest_should_see_login_link(browser):
@@ -14,10 +19,4 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_login_form_and_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-    page.should_be_login_form()
-    page.should_be_register_form()
+
