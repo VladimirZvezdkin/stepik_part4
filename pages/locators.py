0f4dc974d118bebc9049840aaf93492689d840a2
VladimirZvezdkin <vladimir-zvezdkin@yandex.ru>
2022-04-27 18:28:49 +0300

    added new locators for basepage added new tests for login link and login page

--- pages/locators.py
+++ pages/locators.py
@@ -12,6 +12,10 @@ class ProductPageLocators():
     BOOK_NAME = (By.CSS_SELECTOR, '.breadcrumb .active')
     ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
     VIEW_BASKET = (By.XPATH, '//*[@id="default"]/header/div[1]/div/div[2]/span/a')
-    BOOK_ADDED = (By.CSS_SELECTOR, '#messages strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages strong')
     BOOK_PRICE = (By.CSS_SELECTOR, '.col-sm-1 .align-right')
     BASKET_TOTAL = (By.CSS_SELECTOR, '#basket_totals .price_color')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -12,11 +12,20 @@ class ProductPage(BasePage):
 
     def is_book_added_in_basket(self):
         book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
-        message = self.browser.find_element(*ProductPageLocators.BOOK_ADDED).text
+        message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
         assert book_name == message, 'The added to basket book is wrong!'
 
     def is_the_price_correct(self):
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
         assert book_price == basket_total, "The price is not correct!"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappear"
+
 
